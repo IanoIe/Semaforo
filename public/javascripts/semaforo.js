b84753a594b4a4cdaf8e2b7177ea5adf38672e7d
AC_Marvel <gil_1_10@hotmail.com>
2024-03-28 22:32:01 +0100

    Inicialização de criação de js do projeto de Semaforo

---
+++ public/javascripts/semaforo.js
@@ -0,0 +1,14 @@
+const img = document.getElementById('img');
+const buttons = document.getElementById('buttons');
+
+const trafficLght = (event) => {
+    
+}
+
+const turnOn = {
+    'red': () => img.src = './images/vermelho.png',
+    'yellow': () => img.src = './images/amarelo.png',
+    'green': () => img.src = './images/verde.png'
+}
+
+buttons.addEventListener('click', trafficLght);
