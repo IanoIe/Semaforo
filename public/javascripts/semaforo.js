0f361aa261d7d21cf0ad0380345600b906636cfc
AC_Marvel <gil_1_10@hotmail.com>
2024-03-31 13:24:23 +0200

    A funcionalidade automatico do semáforo já funciona perfeitamente, ou seja, o projeto de treinamento esta concluido

--- public/javascripts/semaforo.js
+++ public/javascripts/semaforo.js
@@ -1,15 +1,35 @@
 const img = document.getElementById('img');
 const buttons = document.getElementById('buttons');
 
-const trafficLght = (event) => {
+let colorIndex = 0;
+let setIntervalId  = null;
+
+const trafficLight = (event) => {
+    /**A chamada de stoAutomatic */
+    stopAutomatic();
     turnOn[event.target.id]();
-    
+}
+
+const nextIndex = () => colorIndex = colorIndex < 2 ? ++colorIndex : 0;
+
+
+const changeColor = () => {
+    const colors = ['red', 'yellow', 'green'];
+    const color = colors[colorIndex];
+    turnOn[color]();
+    nextIndex();
+}
+
+
+const stopAutomatic = () => {
+    clearInterval (setIntervalId);
 }
 
 const turnOn = {
     'red': () => img.src = './images/vermelho.png',
     'yellow': () => img.src = './images/amarelo.png',
-    'green': () => img.src = './images/verde.png'
+    'green': () => img.src = './images/verde.png',
+    'automatic': () => setIntervalId = setInterval( changeColor, 1000)
 }
 
-buttons.addEventListener('click', trafficLght);
+buttons.addEventListener('click', trafficLight);
